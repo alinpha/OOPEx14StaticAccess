f8923dba1e050448baac5a9a99a5aecfc3ded906
Aline Vetrov <alinpha@gmail.com>
2020-02-26 13:11:24 -0400

    Create Validate.cs

---
+++ StaticClassDemo/ClassLib/Validate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public static class Validate
+    {
+        static Validate() { }
+
+        public static bool ValidateDecimal(string value)
+        {
+            return Decimal.TryParse(value, out decimal d);
+        }
+
+        public static bool ValidateInteger(string value)
+        {
+            return int.TryParse(value, out int i);
+        }
+
+        public static bool ValidateLength(int lenght, string value)
+        {
+            return value.Length == lenght;
+        }
+
+        public static bool ValidateMaxLength(int lenght, string value)
+        {
+            return value.Length <= lenght;
+        }
+
+        public static bool ValidateMinLength(int lenght, string value)
+        {
+            return value.Length >= lenght;
+        }
+
+    }
+}
