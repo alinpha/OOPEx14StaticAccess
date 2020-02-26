e9459c9a1df8555e6da96b07e1d149d2c5223073
Aline Vetrov <alinpha@gmail.com>
2020-02-26 14:30:31 -0400

    test unit for Validate class completed

--- StaticClassDemo/UnitTestProject1/ValidateTest.cs
+++ StaticClassDemo/UnitTestProject1/ValidateTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLib;
 
 namespace UnitTestProject1
 {
@@ -10,35 +11,62 @@ namespace UnitTestProject1
         [TestMethod]
         public void VlidateDecimalTrue_Pos() 
         {
-            
+            Assert.IsTrue(Validate.ValidateDecimal("10.5"));
         }
 
         [TestMethod]
-        public void ValidateDecimalFalse_Pos() { }
+        public void ValidateDecimalFalse_Pos()
+        {
+            Assert.IsFalse(Validate.ValidateDecimal("a"));
+        }
 
         [TestMethod]
-        public void ValidateIntegerTrue_Pos() { }
+        public void ValidateIntegerTrue_Pos()
+        {
+            Assert.IsTrue(Validate.ValidateInteger("10"));
+        }
 
         [TestMethod]
-        public void ValidateIntegerFalse_Pos() { }
+        public void ValidateIntegerFalse_Pos()
+        {
+            Assert.IsFalse(Validate.ValidateInteger("a"));
+        }
 
         [TestMethod]
-        public void ValidateLengthTrue_Pos() { }
+        public void ValidateLengthTrue_Pos()
+        {
+            Assert.IsTrue(Validate.ValidateLength(2,"aa"));
+        }
 
         [TestMethod]
-        public void ValidateLengthFalse_Pos() { }
+        public void ValidateLengthFalse_Pos()
+        {
+            Assert.IsFalse(Validate.ValidateLength(2, "aaa"));
+        }
 
         [TestMethod]
-        public void ValidateMaxLengthTrue_Pos() { }
+        public void ValidateMaxLengthTrue_Pos()
+        {
+            Assert.IsTrue(Validate.ValidateMaxLength(2,"a"));
+        }
 
         [TestMethod]
-        public void ValidateMaxLengthFalse_Pos() { }
+        public void ValidateMaxLengthFalse_Pos()
+        {
+            Assert.IsFalse(Validate.ValidateMaxLength(2, "aaa"));
+        }
 
         [TestMethod]
-        public void ValidateMinLengthTrue_Pos() { }
+        public void ValidateMinLengthTrue_Pos()
+        {
+            Assert.IsTrue(Validate.ValidateMinLength(2, "aa"));
+        }
 
         [TestMethod]
-        public void ValidateMinLengthFlase_Pos() { }
+        public void ValidateMinLengthFlase_Pos()
+        {
+            Assert.IsFalse(Validate.ValidateMinLength(2, "a"));
+        }
 
     }
 }
