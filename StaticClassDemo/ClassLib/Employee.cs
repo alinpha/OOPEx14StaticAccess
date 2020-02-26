2cff946fac45d843d22366b61057253253da4935
Aline Vetrov <alinpha@gmail.com>
2020-02-26 13:02:28 -0400

    Create Employee.cs

---
+++ StaticClassDemo/ClassLib/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class Employee
+    {
+        private string _empFirstName;
+        private string _empLastName;
+        private string _id;
+        private string _sin;
+
+        public string EmpFirstName 
+        {
+            get;set;
+        }
+
+        public string EmpLastName
+        {
+            get; set;
+        }
+
+        public decimal HourlyRate
+        {
+            get; set;
+        }
+
+        public decimal HoursWorked
+        {
+            get; set;
+        }
+
+        public string ID
+        {
+            get; set;
+        }
+
+        public string SIN
+        {
+            get; set;
+        }
+
+        public Employee() { }
+
+    }
+}
