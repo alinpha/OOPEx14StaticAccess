d257ad73e01168a12bd508468149f738b461492e
Aline Vetrov <alinpha@gmail.com>
2020-02-26 13:30:57 -0400

    rename test unit to ValidateTest

---
+++ StaticClassDemo/UnitTestProject1/ValidateTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ValidateTest
+    {
+
+        [TestMethod]
+        public void VlidateDecimalTrue_Pos() 
+        {
+            
+        }
+
+        [TestMethod]
+        public void ValidateDecimalFalse_Pos() { }
+
+        [TestMethod]
+        public void ValidateIntegerTrue_Pos() { }
+
+        [TestMethod]
+        public void ValidateIntegerFalse_Pos() { }
+
+        [TestMethod]
+        public void ValidateLengthTrue_Pos() { }
+
+        [TestMethod]
+        public void ValidateLengthFalse_Pos() { }
+
+        [TestMethod]
+        public void ValidateMaxLengthTrue_Pos() { }
+
+        [TestMethod]
+        public void ValidateMaxLengthFalse_Pos() { }
+
+        [TestMethod]
+        public void ValidateMinLengthTrue_Pos() { }
+
+        [TestMethod]
+        public void ValidateMinLengthFlase_Pos() { }
+
+    }
+}
